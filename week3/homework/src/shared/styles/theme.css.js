ed2a3d995644f447ef2fc0479b3cbae60e18332b
JS MacBook Pro <j_seokpark@naver.com>
2026-04-29 01:30:12 +0900

    refactor: 스타일 토큰 관리 방식 개선

--- week3/homework/src/shared/styles/theme.css.js
+++ week3/homework/src/shared/styles/theme.css.js
@@ -1,18 +1,14 @@
 import { createGlobalTheme } from "@vanilla-extract/css";
-import {
-  colors,
-  spacing,
-  fontSize,
-  fontWeight,
-  lineHeight,
-} from "./tokens.css";
+import { colors } from "./tokens/color";
+import { typography } from "./tokens/typography";
+import { font } from "./tokens/font";
+import { radius } from "./tokens/radius";
 
 const tokens = {
   color: colors,
-  space: spacing,
-  fontSize,
-  fontWeight,
-  lineHeight,
+  font: font,
+  radius: radius,
+  ...typography,
 };
 
 const themeVars = createGlobalTheme(":root", tokens);
