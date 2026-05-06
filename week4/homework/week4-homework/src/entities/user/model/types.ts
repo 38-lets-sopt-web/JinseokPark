c685bb095af713d1e02fc29210cb0d6ded10eafc
JS MacBook Pro <j_seokpark@naver.com>
2026-05-07 01:36:17 +0900

    feat: user api 함수 구현

---
+++ week4/homework/week4-homework/src/entities/user/model/types.ts
@@ -0,0 +1,27 @@
+export interface User {
+  id: number;
+  loginId: string;
+  name: string;
+  email: string;
+  age: number;
+  part: string;
+}
+
+export type UserSummary = Pick<User, "id" | "name" | "part">;
+
+export type UserUpdateRequest = Pick<User, "name" | "email" | "age" | "part">;
+
+interface BaseResponse<T> {
+  success: boolean;
+  status: number;
+  message: string;
+  code: string;
+  data?: T;
+  meta?: {
+    path: string;
+    timestamp: string;
+  };
+}
+
+export type UserResponse = BaseResponse<User>;
+export type UserListResponse = BaseResponse<UserSummary[]>;
