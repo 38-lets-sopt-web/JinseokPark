ed2136e089695c9c961976f08996fbfb62b6f94f
JS MacBook Pro <j_seokpark@naver.com>
2026-05-07 00:34:17 +0900

    feat: 회원 조회 및 전체 멤버 리스트 페이지 UI 구현

--- week4/homework/week4-homework/src/pages/members/list/MemberListPage.tsx
+++ week4/homework/week4-homework/src/pages/members/list/MemberListPage.tsx
@@ -1,5 +1,47 @@
+import Input from "../../../shared/components/Input";
+import Button from "../../../shared/components/Button";
+import MemberInfoCard from "./components/MemberInfo";
+import MemberCardList from "./components/MemberCardList";
+
 const MemberListPage = () => {
-  return <div>member-list</div>;
+  const mockMembers = [
+    { id: 1, name: "멤버 1", part: "Web" },
+    { id: 2, name: "멤버 2", part: "Web" },
+    { id: 3, name: "멤버 3", part: "Web" },
+    { id: 4, name: "멤버 4", part: "Web" },
+    { id: 5, name: "멤버 5", part: "Web" },
+    { id: 6, name: "멤버 6", part: "Web" },
+  ];
+
+  return (
+    <main className="flex flex-col items-center gap-12 p-8">
+      <section className="flex flex-col w-full max-w-96 gap-6">
+        <h1 className="text-2xl font-bold text-gray-900 text-center">
+          회원 조회
+        </h1>
+        <div className="flex flex-col gap-4">
+          <Input label="회원 ID" type="number" placeholder="ID를 입력하세요" />
+          <Button className="w-full">검색</Button>
+        </div>
+
+        <MemberInfoCard
+          title="검색 결과"
+          data={{
+            username: "jin-evergreen",
+            name: "진석",
+            email: "jin@sopt.org",
+            age: 24,
+            part: "Web",
+          }}
+        />
+      </section>
+
+      <section className="w-full flex flex-col max-w-5xl gap-6">
+        <h2 className="text-xl font-bold text-gray-900">전체 멤버</h2>
+        <MemberCardList members={mockMembers} />
+      </section>
+    </main>
+  );
 };
 
 export default MemberListPage;
