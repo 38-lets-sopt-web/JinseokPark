fd6dcde9df42e66c4ab1b9ef8da7f79401366067
JS MacBook Pro <j_seokpark@naver.com>
2026-05-06 23:56:34 +0900

    feat: 헤더 레이아웃 설정

--- week4/homework/week4-homework/src/app/routes/router.tsx
+++ week4/homework/week4-homework/src/app/routes/router.tsx
@@ -1,15 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../../pages/login/LoginPage";
-import SignUpPage from "../../pages/sign-up/SignupPage";
+import SignUpPage from "../../pages/sign-up/SignUpPage";
 import MyPage from "../../pages/mypage/MyPage";
 import MemberListPage from "../../pages/members/list/MemberListPage";
 import MemberDetailPage from "../../pages/members/detail/MemberDetailPage";
+import Layout from "../layout/Layout";
 
 export const router = createBrowserRouter([
   { path: "/login", element: <LoginPage /> },
   { path: "/signup", element: <SignUpPage /> },
   {
     path: "/mypage",
+    element: <Layout />,
     children: [
       { index: true, element: <MyPage /> },
       {
