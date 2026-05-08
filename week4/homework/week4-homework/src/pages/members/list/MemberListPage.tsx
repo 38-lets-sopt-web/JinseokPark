0defd4373eee0b2f67eeb9aadabb16f0d97e5f07
JS MacBook Pro <j_seokpark@naver.com>
2026-05-08 17:24:27 +0900

    feat: 회원 조회 입력 없을 때 버튼 비활성화

--- week4/homework/week4-homework/src/pages/members/list/MemberListPage.tsx
+++ week4/homework/week4-homework/src/pages/members/list/MemberListPage.tsx
@@ -37,7 +37,7 @@ const MemberListPage = () => {
             value={searchId}
             onChange={(e) => setSearchId(e.target.value)}
           />
-          <Button className="w-full" type="submit">
+          <Button className="w-full" type="submit" disabled={!searchId}>
             검색
           </Button>
         </form>
